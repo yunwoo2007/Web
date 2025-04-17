eb03ffe61358167bdea15275c2573369da00a58c
yunwoo2007 <154660238+yunwoo2007@users.noreply.github.com>
2025-04-17 14:40:36 -0400

    Update f_config.js

--- src/utils/f_config.js
+++ src/utils/f_config.js
@@ -10,16 +10,7 @@
 //   };
 
   
-// //hjm
-// const firebaseConfig = {
-//     apiKey: "AIzaSyC7Aqs0stZxgmBGnvD0ZGeRNmLIMWQHniQ",
-//     authDomain: "jmcoding-27c0b.firebaseapp.com",
-//     projectId: "jmcoding-27c0b",
-//     storageBucket: "jmcoding-27c0b.appspot.com",
-//     messagingSenderId: "66168997581",
-//     appId: "1:66168997581:web:1156d9477cc2423df20b84",
-//     measurementId: "G-3DY71V8Q8X"
-// };
+
 
 //premier
 const firebaseConfig = {
